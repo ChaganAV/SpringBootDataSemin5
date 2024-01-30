05159494e4bac021da392626cab417d60422d9df
Alexandr <jwchaganav@gmail.com>
2024-01-30 13:50:21 +0400

    repo

--- src/main/java/com/example/SptirngBootDataSemin5/repositories/TaskRepository.java
+++ src/main/java/com/example/SptirngBootDataSemin5/repositories/TaskRepository.java
@@ -4,9 +4,10 @@ import com.example.SptirngBootDataSemin5.model.Task;
 import com.example.SptirngBootDataSemin5.model.TaskStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
     //@Query("SELECT t FROM Tasks t WHERE t.status = ?1")
     List<Task> findByStatus(TaskStatus status);
